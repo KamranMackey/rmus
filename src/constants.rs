4a6bd9c39d3278fbdd21fb9245ee25568fa4c484
Kamran Mackey <kamranm1200@gmail.com>
2021-08-05 19:16:48 -0600

    feat: Allow playback of file directories.
    However, the dialog showing the current track has been removed, at least
    until I can figure out how to dynamically update it and make it apply to
    files other than just FLACs.
    
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- src/constants.rs
+++
@@ -1,2 +0,0 @@
-pub const FLAC_ARTIST_TAG: &str = "ARTIST";
-pub const FLAC_TITLE_TAG: &str = "TITLE";
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
 #[macro_use]
 extern crate clap;
 
-mod constants;
-
-use crate::constants::*;
-
 use clap::{App, Arg};
-use claxon::FlacReader;
-use cursive::views::{Dialog, TextView};
-use rodio::{Decoder, OutputStream, Sink};
-use std::ffi::OsStr;
-use std::{fs::File, io::BufReader, path::Path};
+use rodio::{Decoder, OutputStream, OutputStreamHandle, Sink};
+use std::{fs::File, io::BufReader};
 
-fn get_file_extension(name: &str) -> Option<&str> {
-    Path::new(name).extension().and_then(OsStr::to_str)
+struct Player {
+    sink: Sink,
+}
+
+impl Player {
+    pub fn new(handle: &OutputStreamHandle) -> Self {
+        let sink = Sink::try_new(handle);
+        match sink {
+            Ok(sink) => Self { sink },
+            Err(_) => {
+                println!("Failed to initialize new sink.");
+                Self { sink: Sink::new_idle().0 }
+            }
+        }
+    }
+
+    pub fn play_pause(&mut self) {
+        if self.sink.is_paused() {
+            self.sink.play()
+        } else {
+            self.sink.pause()
+        }
+    }
 }
 
 fn main() -> Result<(), std::io::Error> {
@@ -22,30 +36,32 @@ fn main() -> Result<(), std::io::Error> {
         .version_short("v")
         .help_message("Show this help message.")
         .version_message("Show the application's version.")
-        .arg(Arg::with_name("file").help("The input file to play.").required(true).index(1))
+        .arg(Arg::with_name("input").help("The input file to play. Also accepts directories.").required(true).index(1))
         .get_matches();
-
-    let filename = matches.value_of("file").unwrap();
-    let file_ext = get_file_extension(filename).unwrap();
-
-    if file_ext != "flac" {
-        println!("You did not provide a valid FLAC file.");
-        return Ok(());
-    }
 
     let mut siv = cursive::default();
 
     let (_s, handle) = OutputStream::try_default().unwrap();
-    let sink = Sink::try_new(&handle).unwrap();
-    let file = File::open(filename).unwrap();
-    let source = Decoder::new(BufReader::new(file)).unwrap();
-    sink.append(source);
+    let mut player = Player::new(&handle);
+    let input = matches.value_of("input").unwrap();
+    if std::fs::metadata(input)?.is_dir() {
+        let path = std::fs::read_dir(input).unwrap();
+        path.filter_map(Result::ok)
+            .filter_map(|d| d.path().to_str().and_then(|f| if !f.ends_with(".jpg") { Some(d) } else { None }))
+            .for_each(|f| {
+                let filepath = f.path();
+                let path = filepath.as_path().to_str().unwrap();
+                let file = File::open(path).unwrap();
+                let source = Decoder::new(BufReader::new(file)).unwrap();
+                player.sink.append(source);
+            });
+    } else {
+        let file = File::open(input).unwrap();
+        let source = Decoder::new(BufReader::new(file)).unwrap();
+        player.sink.append(source);
+    }
 
-    let reader = FlacReader::open(filename).unwrap();
-    let title = reader.get_tag(FLAC_TITLE_TAG).next().unwrap();
-    let artist = reader.get_tag(FLAC_ARTIST_TAG).next().unwrap();
-
-    siv.add_layer(Dialog::around(TextView::new(format!("Playing {} by {}.", title, artist))));
+    siv.add_global_callback('c', move |_| player.play_pause());
     siv.add_global_callback('q', |s| s.quit());
     siv.run();
 
